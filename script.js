10d1a8301cf2a9924fd094c1a9231948787692e6
devfernando <fernandorufinoalves@gmail.com>
2023-01-30 15:34:07 -0300

    Aplicando funcionalidade de abrir e fechar popup

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
-const closeButton = document.querySelector('.popup-close');
+// const closeButton = document.querySelector('.popup-close');
 
 button.addEventListener('click', () => {
   popup.style.display = 'block';
 })
 
-closeButton.addEventListener('click', () => {
-  popup.style.display = 'none';
+// closeButton.addEventListener('click', () => {
+//   popup.style.display = 'none';
+// })
+
+popup.addEventListener('click', event => {
+  const classNameOfClickedElement = event.target.classList[0]
+  if(classNameOfClickedElement === 'popup-close' || classNameOfClickedElement === 'popup-link' || classNameOfClickedElement === 'popup-wrapper') {
+    popup.style.display = 'none';
+  }
 })
